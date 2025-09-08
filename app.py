5eef129fabb16ea36bac3c680a41ce17da97dc43
Ankit Kumar Singh <ankit1640y@gmail.com>
2025-09-09 00:09:47 +0530

    Trained the Model

--- app.py
+++ app.py
@@ -12,12 +12,29 @@ def pred():
 
 @application.route("/prediction", methods=["POST"])
 def prediction():
-    # Get form data and convert to float
-    float_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(float_features)]
-    
-    # Make prediction.
-    prediction = model.predict(final_features) 
+    # Get form data in the correct order
+    age = float(request.form['Age'])
+    gender = request.form['Gender']
+    country = request.form['Country']
+    self_employed = request.form['self_employed']
+    family_history = request.form['family_history']
+
+    # Encode categorical features (same as training)
+    from sklearn.preprocessing import LabelEncoder
+    le_gender = LabelEncoder().fit(['Male', 'Female', 'Other', 'Unknown'])
+    le_country = LabelEncoder().fit([country])
+    le_self_employed = LabelEncoder().fit(['Yes', 'No', 'Unknown'])
+    le_family_history = LabelEncoder().fit(['Yes', 'No', 'Unknown'])
+
+    gender_encoded = le_gender.transform([gender])[0] if gender in le_gender.classes_ else le_gender.transform(['Unknown'])[0]
+    country_encoded = le_country.transform([country])[0]
+    self_employed_encoded = le_self_employed.transform([self_employed])[0] if self_employed in le_self_employed.classes_ else le_self_employed.transform(['Unknown'])[0]
+    family_history_encoded = le_family_history.transform([family_history])[0] if family_history in le_family_history.classes_ else le_family_history.transform(['Unknown'])[0]
+
+    final_features = np.array([[age, gender_encoded, country_encoded, self_employed_encoded, family_history_encoded]])
+
+    # Make prediction
+    prediction = model.predict(final_features)
 
     # Prepare the prediction message
     if prediction[0] == 1:
